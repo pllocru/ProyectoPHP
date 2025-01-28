d4cdbb8c76213b5bbb0837979b348a8173f50847
Pablo Llorente <161516984+pllocru@users.noreply.github.com>
2025-01-28 08:26:19 +0100

    primera

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Crear 10 usuarios con datos aleatorios
+        User::factory(10)->create();
+    }
+}
